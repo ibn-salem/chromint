17f59015fd4ef54047807f9a5561169d00725b3c
ibn-salem <jonas.ibn-salem@gmx.de>
2017-01-11 19:45:19 +0100

    add package documentation

---
+++ R/chromint.R
@@ -0,0 +1,11 @@
+#'chromint: A package to analyse chromatin interactions.
+#'
+#'This package provides functions to analyse genome-wide chromatin intraction
+#'data, like Hi-C. There are functions to parse high-resolution Hi-C data from
+#'Rao et al. 2014, Cell to quantify intreaction frequencies between pairs of
+#'genomic regions (like genes and enhancers) and randomizations to compute
+#'enrichment of interactions.
+#'@docType package
+#'@name chromint
+NULL
+
