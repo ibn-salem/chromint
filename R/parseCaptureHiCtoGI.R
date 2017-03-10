3bc0251f86832adf4bc15dc7e94b2e6ea43517dc
ibn-salem <jonas.ibn-salem@gmx.de>
2017-03-10 13:28:56 +0100

    add parseCaptureHiC function to parse capture Hi-C data from Mifsud et al. 2015 as GInteraction object.

---
+++ R/parseCaptureHiCtoGI.R
@@ -0,0 +1,54 @@
+
+#' Parse Capture HI-C data from Mifsud et al. 2015 as \code{GInteractions}
+#' object.
+#'
+#' In the paper Mifsud et al. 2015 (https://www.ncbi.nlm.nih.gov/pubmed/25938943)
+#' promoter caputure Hi-C data is provied for two cell types. This data can be
+#' downloaded from the supplemental material of the studay as .zip archive:
+#' http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-2323/E-MTAB-2323.additional.1.zip
+#' Each file in this zip archive can be parsed separately with this function as
+#' an \code{\link[InteractioSet]{GInteractions}}} object.
+#'
+#' @param infile Input file with interactions.
+#' @param seqInfo A seqinfo object holding the length of each chromosome.
+#' @return A \code{\link[InteractioSet]{GInteractions}}} object
+#' @export
+parseCaptureHiC <- function(infile, seqInfo=NULL){
+
+  # get classes of each column for faster parsing
+  classes <- sapply(read.delim(infile,
+                                nrows = 5,
+                                header = TRUE,
+                                stringsAsFactors = FALSE ), class)
+
+  # parse input file as data frame
+  df <- read.delim(infile,
+                       header = TRUE,
+                       colClasses = classes,
+                       stringsAsFactors = FALSE)
+
+  gr1 <- GenomicRanges::GRanges(
+      df$chr,
+      IRanges::IRanges(df$start, df$end),
+      seqinfo = seqInfo,
+      symbol = df$Symbol,
+      ENSG = df$Ensembl.Gene.ID,
+      expresssion.quartile = df$expresssion.quartile)
+  gr2 <- GenomicRanges::GRanges(
+    df$chr.1,
+    IRanges::IRanges(df$start.1, df$end.1),
+    seqinfo = seqInfo,
+    symbol = df$Symbol.1,
+    ENSG = df$Ensembl.Gene.ID.1,
+    expresssion.quartile = df$expresssion.quartile.1)
+
+  # construct GInteraction
+  gi <- InteractionSet::GInteractions(
+    gr1,
+    gr2,
+    raw.count = df$raw.count,
+    log.observed.expected = df$log.observed.expected
+  )
+
+  return(gi)
+}
