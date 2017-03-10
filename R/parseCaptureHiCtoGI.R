8c4beb899d61dddfc8912b3188bb4717957b9921
ibn-salem <jonas.ibn-salem@gmx.de>
2017-03-10 13:44:14 +0100

    fix documentation for parseCaptureHiC function

--- R/parseCaptureHiCtoGI.R
+++ R/parseCaptureHiCtoGI.R
@@ -1,17 +1,17 @@
 
-#' Parse Capture HI-C data from Mifsud et al. 2015 as \code{GInteractions}
-#' object.
+#' Parse Capture Hi-C data as \code{GInteractions} object.
 #'
-#' In the paper Mifsud et al. 2015 (https://www.ncbi.nlm.nih.gov/pubmed/25938943)
-#' promoter caputure Hi-C data is provied for two cell types. This data can be
-#' downloaded from the supplemental material of the studay as .zip archive:
-#' http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-2323/E-MTAB-2323.additional.1.zip
+#' In the paper Mifsud et al. 2015
+#' (\url{https://www.ncbi.nlm.nih.gov/pubmed/25938943}) promoter caputure Hi-C
+#' data is provied for two cell types. This data can be downloaded from the
+#' supplemental material of the studay as .zip archive:
+#' \url{http://www.ebi.ac.uk/arrayexpress/files/E-MTAB-2323/E-MTAB-2323.additional.1.zip}.
 #' Each file in this zip archive can be parsed separately with this function as
-#' an \code{\link[InteractioSet]{GInteractions}}} object.
+#' an \code{\link[InteractioSet]{GInteractions}} object.
 #'
 #' @param infile Input file with interactions.
 #' @param seqInfo A seqinfo object holding the length of each chromosome.
-#' @return A \code{\link[InteractioSet]{GInteractions}}} object
+#' @return A \code{\link[InteractioSet]{GInteractions}} object
 #' @export
 parseCaptureHiC <- function(infile, seqInfo=NULL){
 
